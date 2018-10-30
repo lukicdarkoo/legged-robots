cbd2ce61c6aec05ec40640aa4892abf240ef2722
Leonardo Cencetti <leonardo.cencetti@epfl.ch>
2018-10-30 14:16:22 +0100

    eval Am and eval Ap completed

--- dynamics/eval_A_m.m
+++ dynamics/eval_A_m.m
@@ -1,3 +1,7 @@
 function A_m = eval_A_m(q_m)
-
+[m1, m2, m3, l1, l2, l3, g] = set_parameters;
+t2 = cos(q1_m);
+t3 = abs(l1.*t2.*(1.0./2.0)-l2.*cos(q2_m));
+t4 = abs(l1.*sin(q1_m).*(1.0./2.0)-l2.*sin(q2_m));
+A_m = [l1.*m.*t2.*sqrt(t3.^2+t4.^2).*(-1.0./2.0),0.0,l3.^2.*m3.*cos(q3_m).*(1.0./4.0)];
 end
--- dynamics/eval_A_p.m
+++ dynamics/eval_A_p.m
@@ -1,3 +1,4 @@
 function A_p = eval_A_p(q_p)
-
+[m1, m2, m3, l1, l2, l3, g] = set_parameters;
+A_p = [l1.*l2.*m.*cos(q1_p).*(-1.0./4.0),l1.*l2.*m.*cos(q2_p).*(-1.0./4.0),l3.^2.*m3.*cos(q3_p).*(1.0./4.0)];
 end
