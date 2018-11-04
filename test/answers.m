87a1db0fda8019595b61b89c365952307983aba3
Darko Lukic <lukicdarkoo@gmail.com>
2018-11-04 22:20:14 +0100

    Add answers

--- test/answers.m
+++ test/answers.m
@@ -1,6 +1,10 @@
-function test_kinetic_loss()
-    % Question 2
-    fprintf('Question 2\n');
+function answers()
+    % Answer 1
+    fprintf('Answer 1\n');
+    fprintf('The potential energy is the same before and after impact\n\n');
+
+    % Answer 2
+    fprintf('Answer 2\n');
     q_m = [pi/6, -pi/6, pi/10];
     dq_m = [1, 0.2, 0];
     
@@ -9,10 +13,10 @@ function test_kinetic_loss()
     [kinetic_p, potential_p] = eval_energy(q_p, dq_p);
 
     energy_loss_percent = (1 - kinetic_p / kinetic_m) * 100;
-    fprintf('Energy loss = %f\n\n', energy_loss_percent);
+    fprintf('Energy loss = %f percent\n\n', energy_loss_percent);
     
-    % Question 3
-    fprintf('Question 2\n');
+    % Answer 3
+    fprintf('Answer 3\n');
     energy_loss_vector = [];
     i = 1;
     for alpha=0:0.01:pi/4
@@ -28,4 +32,9 @@ function test_kinetic_loss()
         i = i + 1;
     end
     plot(0:0.01:pi/4, energy_loss_vector);
+    fprintf('\n');
+    
+    % Answer 4
+    fprintf('Answer 4\n');
+    fprintf('Bigger step length increases a kinetic energy loss\n');
 end
